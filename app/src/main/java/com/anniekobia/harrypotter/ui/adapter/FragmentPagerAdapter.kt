785c7fa7c1b1369ad7bb43290000c85be0974354
Anniekobia <kobiaannunziata94@gmail.com>
2020-08-26 03:52:50 +0300

    Separated FragmentPagerAdapter class to a different file for code readability

---
+++ app/src/main/java/com/anniekobia/harrypotter/ui/adapter/FragmentPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.anniekobia.harrypotter.ui.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class FragmentPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    private val mFragmentList: ArrayList<Fragment> = ArrayList()
+    private val mFragmentTitleList: ArrayList<String> = ArrayList()
+
+    override fun getItem(position: Int): Fragment {
+
+        return mFragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mFragmentTitleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title : String) {
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+}
--- app/src/main/java/com/anniekobia/harrypotter/ui/view/HomeFragment.kt
+++ app/src/main/java/com/anniekobia/harrypotter/ui/view/HomeFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 
 import com.anniekobia.harrypotter.R
@@ -27,7 +25,7 @@ class HomeFragment : Fragment() {
         tabs = view.findViewById(R.id.tab_layout)
         viewPager = view.findViewById(R.id.viewPager)
 
-        val adapter = MyFragmentPagerAdapter(childFragmentManager)
+        val adapter = com.anniekobia.harrypotter.ui.adapter.FragmentPagerAdapter(childFragmentManager)
         adapter.addFragment(AllCharactersFragment(),getString(R.string.all_fragment_tab_title))
         adapter.addFragment(StudentsFragment(),getString(R.string.students_fragment_tab_title))
         adapter.addFragment(StaffFragment(),getString(R.string.staff_fragment_tab_title))
@@ -36,28 +34,5 @@ class HomeFragment : Fragment() {
         tabs.setupWithViewPager(viewPager)
 
         return view
-    }
-
-    private inner class MyFragmentPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-        private val mFragmentList: ArrayList<Fragment> = ArrayList()
-        private val mFragmentTitleList: ArrayList<String> = ArrayList()
-
-        override fun getItem(position: Int): Fragment {
-
-            return mFragmentList[position]
-        }
-
-        override fun getCount(): Int {
-            return mFragmentList.size
-        }
-
-        override fun getPageTitle(position: Int): CharSequence? {
-            return mFragmentTitleList[position]
-        }
-
-        fun addFragment(fragment: Fragment,title : String) {
-            mFragmentList.add(fragment)
-            mFragmentTitleList.add(title)
-        }
     }
 }
