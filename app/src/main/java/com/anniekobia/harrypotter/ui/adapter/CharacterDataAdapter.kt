8003cf8cf6decf8e081b578993e59538c0785a0e
Anniekobia <kobiaannunziata94@gmail.com>
2020-08-31 15:26:50 +0300

    viewbinding in adapter class

--- app/src/main/java/com/anniekobia/harrypotter/ui/adapter/CharacterDataAdapter.kt
+++ app/src/main/java/com/anniekobia/harrypotter/ui/adapter/CharacterDataAdapter.kt
@@ -2,13 +2,12 @@ package com.anniekobia.harrypotter.ui.adapter
 
 import android.os.Build
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.anniekobia.harrypotter.R
 import com.anniekobia.harrypotter.data.api.model.Character
+import com.anniekobia.harrypotter.databinding.CharacterItemRowBinding
 import com.squareup.picasso.Picasso
 
 
@@ -17,11 +16,12 @@ class CharacterDataAdapter(
         private val listener: (Character, ImageView) -> Unit
 ) : RecyclerView.Adapter<CharacterDataAdapter.MyViewHolder>() {
 
+    private lateinit var binding: CharacterItemRowBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView: View =
-                LayoutInflater.from(parent.context).inflate(R.layout.character_item_row, parent, false)
-        return MyViewHolder(itemView)
+        binding = CharacterItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MyViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -46,9 +46,9 @@ class CharacterDataAdapter(
         holder.itemView.setOnClickListener { listener(character,holder.characterImage) }
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val characterImage: ImageView = itemView.findViewById(R.id.character_image)
-        val characterName: TextView = itemView.findViewById(R.id.character_name)
-        val characterActorName: TextView = itemView.findViewById(R.id.character_actor_name)
+    class MyViewHolder(itemBinding: CharacterItemRowBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+        val characterImage: ImageView = itemBinding.characterImage
+        val characterName: TextView = itemBinding.characterName
+        val characterActorName: TextView = itemBinding.characterActorName
     }
 }
